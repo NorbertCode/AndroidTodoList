e66bc822dfd7cb4ec1a7f9a9cdba51f5b45813a7
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-04-30 20:33:01 +0200

    task options view changes

--- app/src/main/java/com/example/todolist/TaskOptions.kt
+++ app/src/main/java/com/example/todolist/TaskOptions.kt
@@ -1,11 +1,32 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityTaskOptionsBinding
 
 class TaskOptions : AppCompatActivity() {
+    private lateinit var binding: ActivityTaskOptionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task_options)
+        binding = ActivityTaskOptionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (intent.hasExtra("TASK_NAME")) {
+            binding.etName.setText(intent.getStringExtra("TASK_NAME"))
+        }
+
+        binding.btDiscard.setOnClickListener { exit() }
+        binding.btSave.setOnClickListener { save() }
+    }
+
+    private fun exit() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun save() {
+        // todo: save
+        exit()
     }
 }
